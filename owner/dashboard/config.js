b0c8a0587766c6f67d9be2fc2f6e4803d611d9b2
Loker Kendari Official <lokerkendariofficial@gmail.com>
2026-06-12 16:53:58 +0800

    Create config.js

---
+++ owner/dashboard/config.js
@@ -0,0 +1,10 @@
+// ===========================================
+// FILE: config.js
+// Konfigurasi dan variabel global
+// ===========================================
+const ACCESS_CODE = '900900';
+const PASSWORD = '900900';
+const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5xtxnptjxh-FIyJuBoCct5vudZFFOeaisUUhT8X0R0kjAkY3f2CtD45CYcZjnPxGh52Og7_AXA756/pub?output=csv';
+
+let rawJobs = [];
+let monthlyChart, locationBarChart, typePieChart, trendLineChart;
